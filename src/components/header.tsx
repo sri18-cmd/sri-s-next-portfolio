ce5a2f49ea9bae89e5c70ad1b2bda037b9368872
VUTUKURI SRI VASANTH <srivasanthvutukuri18@gmail.com>
2025-08-06 23:55:56 +0530

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ const navLinks = [
   { name: 'Home', href: '#home' },
   { name: 'About', href: '#about' },
   { name: 'Projects', href: '#projects' },
-  { name: 'Resume', href: '#resume' },
+  { name: 'Experience', href: '#experience' },
   { name: 'Contact', href: '#contact' },
 ];
 
